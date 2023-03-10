47da6aad6e897c90b586c6c9f43304f29822e385
Bilgehan Denizli <denizlibilgehan@gmail.com>
2023-03-10 13:58:15 +0300

    grpc

---
+++ kitaplik-common/src/test/java/com/kitaplik/kitaplikcommon/KitaplikCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitaplik.kitaplikcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitaplikCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
